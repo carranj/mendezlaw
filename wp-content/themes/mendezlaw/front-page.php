529f009013cb2cdb64d65effe522552ae70b791f
carranj <carranco.jose.r@gmail.com>
2021-08-16 23:19:12 -0500

    updated internal page with header and basic gutenberg integration

--- wp-content/themes/mendezlaw/front-page.php
+++ wp-content/themes/mendezlaw/front-page.php
@@ -2,9 +2,53 @@
 get_header();
 
 ?>
-<section id="intro">
-
+<section id="intro" style="background-image:url(<?php echo content_url('/uploads/2021/08/Housing-Subdivision-Or-Housing.jpg')?>); background-size:cover;">
+    <div class="overlay px-4 d-flex align-items-center">
+        <div class="row">
+            <div class="col-md-12">
+                <p>Communication is key</p>
+                <a href="" class='btn btn-primary btn-lg'>Schedule an appointment</a>
+            </div>
+        </div>
+    </div>
 </section>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Meet the team</h2>
+        </div>
+        <div class="col-md-4">
+            <div class="profile">
+            <img class="img-fluid" src= "<?php echo content_url('/uploads/2021/08/profile-placholder.png')?>" alt="">
+            </div>
+            <div class="signature">
+                <p>Terri C. Mendez</p>
+                <p>Attorney</p>
+            </div>
+
+        </div>
+        <div class="col-md-4">
+            <div class="profile">
+            <img class="img-fluid" src= "<?php echo content_url('/uploads/2021/08/profile-placholder.png')?>" alt="">
+            </div>
+            <div class="signature">
+                <p>Terri C. Mendez</p>
+                <p>Attorney</p>
+            </div>
+
+        </div>
+        <div class="col-md-4">
+            <div class="profile">
+                <img class="img-fluid" src= "<?php echo content_url('/uploads/2021/08/profile-placholder.png')?>" alt="">
+            </div>
+            <div class="signature">
+                <p>Terri C. Mendez</p>
+                <p>Attorney</p>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 
 <?php get_footer(); ?>
--- wp-content/themes/mendezlaw/page.php
+++ wp-content/themes/mendezlaw/page.php
@@ -1,10 +1,30 @@
 <?php get_header(); ?>
 
-	<div <?php post_class('container'); ?>>
+	<div <?php post_class('internal-page'); ?>>
 
-		<?php
-			get_template_part( 'page-templates/flexible_content/flexible_content' );
-		?>
+		<?php while ( have_posts() ) : $featuredImage = get_the_post_thumbnail_url(); ?>
+		<section class='internal-heading' style="background-image:url(<?php echo $featuredImage; ?>); background-size:cover;">
+			<div class="overlay py-5 d-flex align-items-center">
+				<div class="container">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</div>
+			<div class="textured-bottom"></div>
+
+		</section>
+		<div class='container'>
+			<?php 
+				the_post();
+				the_content();
+			
+				// If comments are open or there is at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+			endwhile; // End of the loop.
+
+			?>
+		</div>
 		
 
 		
